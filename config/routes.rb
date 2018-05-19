183363e91dac81d80e33b66ce74d4caf646e8f35
Shun Takeuchi <takeuchishun89@gmail.com>
2018-05-19 18:59:06 +0900

    Add: user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'todos#index'
-  get 'signup' => 'users#new'
 
-  get 'page/hello'
+
+  get 'signup' => 'users#new'
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
