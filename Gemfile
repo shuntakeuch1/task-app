6b476efb83ff313a44cb83346a44c5fbfceeae4d
Shun Takeuchi <takeuchishun89@gmail.com>
2018-05-19 14:42:03 +0900

    Add: ユーザーコントローラー

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'foreman'
+  gem 'rubocop'
+  gem 'ruby-lint'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'todos/index'
+  root 'todos#index'
+  get 'signup' => 'users#new'
 
   get 'page/hello'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
