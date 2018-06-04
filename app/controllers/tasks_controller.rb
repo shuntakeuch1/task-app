459d6fcafad0ee00cbdf01584369cd2c39767a7b
Shun Takeuchi <takeuchishun89@gmail.com>
2018-06-04 11:02:01 +0900

    Add: タスク追加処理

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,11 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to @task
-    else
-      render 'new'
+      # すべての結果を返す
+      @tasks = Task.where(project_id: @task.project_id)
+      # redirect_to controller: "project" ,action:"show", id: task_params.project_id
+    # else
+    #   render 'new'
     end
   end
 
@@ -24,7 +26,7 @@ class TasksController < ApplicationController
   private
 
     def task_params
-      params.require(:task).permit(:user_id,:title,:body,:is_completed)
+      params.require(:task).permit(:project_id,:title,:body,:is_completed)
     end
 
 end
